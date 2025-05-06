773c8108e4a8c65982ee0de99497c3b177629639
kit-hong <kithong1999@gmail.com>
2025-05-07 01:13:33 +0800

    added create_df

---
+++ data/create_dataframe.py
@@ -0,0 +1,37 @@
+import os
+import pandas as pd
+
+def create_mapping(labels):
+    label2idx = {label:idx for idx,label in enumerate(labels)}
+    idx2label = {idx:label for idx,label in enumerate(labels)}
+    return label2idx, idx2label
+
+def create_dataframe(path, labels, label2idx, ALLOWED_EXTENSIONS = ('jpg', 'jpeg', 'png')):
+  data = []
+
+  for foldername in os.listdir(path):
+    folderpath = os.path.join(path,foldername)
+
+    if foldername in labels and os.path.isdir(folderpath):
+
+      data.extend(
+          # (imagepaths, labels)
+          (os.path.join(folderpath,image), label2idx[foldername])
+          for image in os.listdir(folderpath)
+          if os.path.isfile(os.path.join(folderpath,image)) and image.lower().endswith(ALLOWED_EXTENSIONS)
+      )
+
+  df = pd.DataFrame(data, columns = ['imagepath', 'label'])
+  return df
+
+def main():
+    labels = ["Unripe", "Early Ripening", "Ripe", "Fully Ripe", "Overripe"]
+    label2idx, idx2label = create_mapping(labels)
+
+    data_path = "/output"
+    
+    df = create_dataframe(data_path, labels, label2idx)
+    df.to_csv(os.path.join(os.getcwd(), 'data.csv'), index=False)
+
+if __name__ == "__main__":
+    main()
